c1c194d6ff24183f87ee8bb8858094cf58a77e24
Daniel Gonçalves <d.aguiargoncalves@gmail.com>
2018-11-11 15:26:13 -0200

    ex 1,2 e 3 feitos

--- music_project/app_music/models.py
+++ music_project/app_music/models.py
@@ -15,12 +15,19 @@ class Musico(models.Model):
 	data_nasc = models.DateField()
 
 	def __str__(self):
-		return self.nome + " " + str(self.cpf) + " " + str(data_nasc) + " " + str(salario)
+		return self.nome + " " + str(self.cpf) + " " + str(self.data_nasc) + " " + str(self.salario)
 
 class Banda(models.Model):
 	nome = models.CharField(max_length=255)
 	estilo_musical = models.ForeignKey(EstiloMusical, on_delete = models.CASCADE)
 	musicos = models.ManyToManyField(Musico)
 
+	def _nome_musicos(self):
+		nomes = ""
+		for musico in self.musicos:
+			nomes += musico.nome
+
+		return nomes
+
 	def __str__(self):
-		return self.nome
+		return self.nome + " Estilo: " + str(self.estilo_musical) + " Musicos: " + str(self.musicos)
